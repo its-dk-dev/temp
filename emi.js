a11ccb429117b41cddeec6c5a5661ea34d5d544f
Dnyaneshwar B. Kanawade <43498173+its-dk-dev@users.noreply.github.com>
2022-11-25 16:39:48 +0530

    Update emi.js

--- emi.js
+++ emi.js
@@ -2,8 +2,6 @@ function calculateEMI(loan) {
             let interst_rate=8.5
             let tenure=15
             let r = interst_rate / 12 / 100
-
-            // loan * r * Math.pow((1+ r), tenure) / [Math.pow((1+r), tenure) - 1]
             // let interest_rate_per_year=loan*8.5/100
             // let emi_message = "Principle amount is: " + loan
             // let totalEmiAmountPerYear = (loan/15) + interest_rate_per_year
